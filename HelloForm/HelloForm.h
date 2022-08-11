d21889f032c812827cebacc88eb0fe9614b8ec51
Felipe Vidal <felipeisra.vidal@gmail.com>
2022-08-11 13:51:05 -0300

    Initial Commit

---
+++ HelloForm/HelloForm.h
@@ -0,0 +1,18 @@
+//
+//  HelloForm.h
+//  HelloForm
+//
+//  Created by Felipe Vidal on 11/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloForm.
+FOUNDATION_EXPORT double HelloFormVersionNumber;
+
+//! Project version string for HelloForm.
+FOUNDATION_EXPORT const unsigned char HelloFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloForm/PublicHeader.h>
+
+
